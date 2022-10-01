6a985297b0f4feeaf6e79b54d8c8d4ec30c11855
Andre Prolo <proloandre@hotmail.com>
2022-10-01 10:12:23 -0300

    initial commit

---
+++ dwm-bar/bar-functions/dwm_pulse.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# A dwm_bar function to show the master volume of PulseAudio
+# Joe Standring <git@joestandring.com>
+# GNU GPLv3
+
+# Dependencies: pamixer
+
+dwm_pulse () {
+    VOL=$(pamixer --get-volume)
+    STATE=$(pamixer --get-mute)
+    
+    printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "unicode" ]; then
+        if [ "$STATE" = "true" ] || [ "$VOL" -eq 0 ]; then
+            printf "ﱝ "
+        elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+            printf " %s%%" "$VOL"
+        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+            printf "奔 %s%%" "$VOL"
+        else
+            printf "墳 %s%%" "$VOL"
+        fi
+    else
+        if [ "$STATE" = "true" ] || [ "$VOL" -eq 0 ]; then
+            printf "MUTE"
+        elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+            printf "VOL %s%%" "$VOL"
+        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+            printf "VOL %s%%" "$VOL"
+        else
+            printf "VOL %s%%" "$VOL"
+        fi
+    fi
+    printf "%s\n" "$SEP2"
+}
+
+dwm_pulse
